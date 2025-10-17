5914e05e96246764a8ddb4ee1049de5a2024e5b3
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-17 20:36:12 +0530

    Add markup for PokemonCard.

---
+++ src/lib/features/info/PokemonCard.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	/*
+    "1": {
+		"id": 1,
+		"name": "Bulbasaur",
+		"type": ["Grass", "Poison"],
+		"hp": 45,
+		"attack": 49,
+		"defense": 49,
+		"special attack": 65,
+		"special defense": 65,
+		"speed": 45,
+		"species": "Seed Pokémon",
+		"description": "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun’s rays, the seed grows progressively larger.",
+		"height": "0.7 m",
+		"weight": "6.9 kg",
+		"gender": "87.5:12.5"
+	}
+    */
+</script>
+
+<article aria-labelledby="pokemon-name-1">
+	<div>
+		<h2 id="pokemon-name-1">
+			<a href="/1">Bulbasaur</a>
+		</h2>
+
+		<ul aria-label="Bulbasaur belongs to the types:">
+			<li>Grass</li>
+			<li>Poison</li>
+		</ul>
+
+		<div>
+			<div aria-label="Weight">
+				<span>6.9 kg</span>
+			</div>
+
+			<div aria-label="Height">
+				<span>0.7 m</span>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<img src="/assets/thumbnails/001.webp" alt="" height="96" width="96" />
+	</div>
+</article>
