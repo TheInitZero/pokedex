4ca2fc1d54e2e49e78ab7cc50a4ae7639f0689d2
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-17 19:57:25 +0530

    Add title and description meta tag.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,12 @@
 </script>
 
 <svelte:head>
+	<title>Pokédex</title>
 	<link rel="icon" href="/assets/pokeball.svg" type="image/svg+xml" />
+	<meta
+		name="description"
+		content="Explore the ultimate Pokédex! Search, browse, and learn about every Pokémon, their stats, types and more. Perfect for trainers and fans alike."
+	/>
 </svelte:head>
 
 <Header />
