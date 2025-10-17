cd0d305a1f79b414582ea3ba676380c24d05717c
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-17 14:37:56 +0530

    Add markup for error pages.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<section aria-labelledby="error-section-heading">
+	<h1 id="error-section-heading">Error: {page.status}</h1>
+	<p>{page.error?.message ?? 'Something went wrong'}</p>
+	<a href="/">Go back to home page</a>
+</section>
