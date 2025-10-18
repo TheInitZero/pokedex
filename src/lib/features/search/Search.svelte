e1f4e4f1f9e717b74e939b9f5cdb5547670ed517
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-18 19:35:14 +0530

    Add styles to Search.

--- src/lib/features/search/Search.svelte
+++ src/lib/features/search/Search.svelte
@@ -23,6 +23,7 @@
 
 {#snippet iconSearch()}
 	<svg
+		class="icon"
 		aria-hidden="true"
 		height="128"
 		width="128"
@@ -37,15 +38,15 @@
 	</svg>
 {/snippet}
 
-<form role="search">
-	<div>
+<form role="search" class="form">
+	<div class="form__group form__group--1">
 		<label for="search-name">Name:</label>
-		<input type="text" name="search-name" id="search-name" />
+		<input class="input" type="text" name="search-name" id="search-name" />
 	</div>
 
-	<div>
+	<div class="form__group form__group--2">
 		<label for="search-type">Type:</label>
-		<select name="search-type" id="search-type">
+		<select class="select" name="search-type" id="search-type">
 			<option value="All">All</option>
 
 			{#each pokemonTypes as pokemonType}
@@ -54,10 +55,69 @@
 		</select>
 	</div>
 
-	<div>
-		<button type="submit">
-			Search
+	<div class="form__group form__group--3">
+		<button class="button" type="submit">
+			<span>Search</span>
 			{@render iconSearch()}
 		</button>
 	</div>
 </form>
+
+<style>
+	.form {
+		display: grid;
+		gap: 1.5rem;
+	}
+
+	.form__group {
+		display: grid;
+		gap: 0.5rem;
+	}
+
+	.icon {
+		height: 1rem;
+		width: 1rem;
+		fill: white;
+	}
+
+	.input,
+	.select {
+		padding: 0.5em;
+		border: 2px solid black;
+		border-radius: 0.5em;
+	}
+
+	.button {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+		justify-content: center;
+		padding: 0.5em;
+		border: 2px solid black;
+		border-radius: 0.5em;
+		background-color: black;
+		color: white;
+	}
+
+	@media (min-width: 475px) {
+		.form {
+			grid-template-columns: repeat(2, 1fr);
+		}
+
+		.form__group--3 {
+			grid-column: span 2;
+		}
+	}
+
+	@media (min-width: 768px) {
+		.form {
+			grid-template-columns: 2fr 2fr 1fr;
+		}
+
+		.form__group--3 {
+			grid-column: span 1;
+			justify-items: stretch;
+			align-items: end;
+		}
+	}
+</style>
