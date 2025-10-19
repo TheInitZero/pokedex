ee4ac158451d9e54ef1f5de29d9c27bfdd8c5f61
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-19 13:48:43 +0530

    Style PokemonTypes.

--- src/lib/features/info/PokemonTypes.svelte
+++ src/lib/features/info/PokemonTypes.svelte
@@ -1,10 +1,38 @@
-<ul aria-label="Bulbasaur belongs to the types:">
-	<li>
-		<img src="/assets/typeIcons/grass.svg" alt="" height="24" width="24" />
-		<span>Grass</span>
+<ul class="type-list" aria-label="Bulbasaur belongs to the types:">
+	<li class="type-list__item">
+		<img class="type-list__icon" src="/assets/typeIcons/grass.svg" alt="" height="48" width="48" />
+		<span class="type-list__name">Grass</span>
 	</li>
-	<li>
-		<img src="/assets/typeIcons/poison.svg" alt="" height="24" width="24" />
-		<span>Poison</span>
+	<li class="type-list__item">
+		<img class="type-list__icon" src="/assets/typeIcons/poison.svg" alt="" height="48" width="48" />
+		<span class="type-list__name">Poison</span>
 	</li>
 </ul>
+
+<style>
+	.type-list {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
+	}
+
+	.type-list__item {
+		background-color: black;
+		color: white;
+		padding: 0.5em;
+		border-radius: 0.5em;
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	.type-list__icon {
+		height: 1rem;
+		width: 1rem;
+	}
+
+	.type-list__name {
+		font-size: var(--font-size-sm);
+	}
+</style>
