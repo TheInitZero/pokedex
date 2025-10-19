e9aaf0511c3fe5bf8610affd8b6bac10a60aee1f
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-19 14:56:49 +0530

    Create PokemonCardGrid.

---
+++ src/lib/features/info/PokemonCardGrid.svelte
@@ -0,0 +1,26 @@
+<script>
+	import PokemonCard from './PokemonCard.svelte';
+</script>
+
+<ul class="list">
+	{#each { length: 12 }, index (index)}
+		<li class="list__item">
+			<PokemonCard />
+		</li>
+	{/each}
+</ul>
+
+<style>
+	.list {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		gap: 1rem;
+		margin-block-start: 2em;
+	}
+
+	.list__item {
+		list-style-type: none;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+	import PokemonCardGrid from '$lib/features/info/PokemonCardGrid.svelte';
 	import Pagination from '$lib/features/pagination/Pagination.svelte';
 </script>
+
+<PokemonCardGrid />
 
 <Pagination />
