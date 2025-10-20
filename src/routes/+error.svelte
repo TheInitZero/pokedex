3499f4cf6e23163e12cbf3789adee30307d799ad
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-20 14:12:17 +0530

    Style error page.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,8 +2,35 @@
 	import { page } from '$app/state';
 </script>
 
-<section aria-labelledby="error-section-heading">
-	<h1 id="error-section-heading">Error: {page.status}</h1>
-	<p>{page.error?.message ?? 'Something went wrong'}</p>
-	<a href="/">Go back to home page</a>
+<section class="error" aria-labelledby="error-section-heading">
+	<h2 class="error__title" id="error-section-heading">Error: {page.status}</h2>
+	<img class="error__image" src="/assets/images/404.png" alt="" height="160" width="160" />
+	<p class="error__message">{page.error?.message ?? 'Something went wrong'}</p>
+	<a class="error__link" href="/">Go back to home page</a>
 </section>
+
+<style>
+	.error {
+		margin-block-start: 1em;
+		border: 2px solid black;
+		padding: 1em;
+		border-radius: 1em;
+		margin-inline: auto;
+		max-width: fit-content;
+		text-align: center;
+	}
+
+	.error__message {
+		margin-block-start: 0.5rem;
+	}
+
+	.error__link {
+		margin-block-start: 0.5rem;
+		display: inline-block;
+		padding: 0.5em;
+		border: 2px solid black;
+		border-radius: 0.5em;
+		background-color: black;
+		color: white;
+	}
+</style>
