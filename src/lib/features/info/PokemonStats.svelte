5cc92525baaf0bed648b5a9b8c0af39be8dfb000
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-20 13:58:25 +0530

    Style PokemonStats.

--- src/lib/features/info/PokemonStats.svelte
+++ src/lib/features/info/PokemonStats.svelte
@@ -1,36 +1,78 @@
-<section aria-labelledby="stats-section-heading">
+<section class="section" aria-labelledby="stats-section-heading">
 	<h3 id="stats-section-heading">Stats</h3>
 
-	<ul aria-labelledby="stats-section-heading">
-		<li>
-			<i class="ri-heart-line"></i>
-			<span>HP</span>
-			<span>45</span>
+	<ul class="stats" aria-labelledby="stats-section-heading">
+		<li class="stat">
+			<h4 class="stat__heading text-base">HP</h4>
+			<p class="stat__point text-h5">45</p>
+			<i class="stat__icon ri-heart-line"></i>
 		</li>
-		<li>
-			<i class="ri-sword-line"></i>
-			<span>Attack</span>
-			<span>49</span>
+		<li class="stat">
+			<h4 class="stat__heading text-base">Attack</h4>
+			<p class="stat__point text-h5">49</p>
+			<i class="stat__icon ri-sword-line"></i>
 		</li>
-		<li>
-			<i class="ri-shield-line"></i>
-			<span>Defense</span>
-			<span>49</span>
+		<li class="stat">
+			<h4 class="stat__heading text-base">Defense</h4>
+			<p class="stat__point text-h5">49</p>
+			<i class="stat__icon ri-shield-line"></i>
 		</li>
-		<li>
-			<i class="ri-sword-fill"></i>
-			<span>Special Attack</span>
-			<span>65</span>
+		<li class="stat">
+			<h4 class="stat__heading text-base">Special Attack</h4>
+			<p class="stat__point text-h5">65</p>
+			<i class="stat__icon ri-sword-fill"></i>
 		</li>
-		<li>
-			<i class="ri-shield-fill"></i>
-			<span>Special Defense</span>
-			<span>65</span>
+		<li class="stat">
+			<h4 class="stat__heading text-base">Special Defense</h4>
+			<p class="stat__point text-h5">65</p>
+			<i class="stat__icon ri-shield-fill"></i>
 		</li>
-		<li>
-			<i class="ri-dashboard-3-line"></i>
-			<span>Speed</span>
-			<span>45</span>
+		<li class="stat">
+			<h4 class="stat__heading text-base">Speed</h4>
+			<p class="stat__point text-h5">45</p>
+			<i class="stat__icon ri-dashboard-3-line"></i>
 		</li>
 	</ul>
 </section>
+
+<style>
+	.section {
+		margin-block-start: 1em;
+	}
+
+	.stats {
+		margin-block-start: 1em;
+		display: grid;
+		gap: 1rem;
+		grid-template-columns: repeat(2, 1fr);
+
+		@media (min-width: 768px) {
+			grid-template-columns: repeat(3, 1fr);
+		}
+	}
+
+	.stat {
+		position: relative;
+		list-style-type: none;
+		display: grid;
+		padding: 1em;
+		border: 2px solid black;
+		border-radius: 1em;
+	}
+
+	.stat__point {
+		margin-block-start: 0.5rem;
+	}
+
+	.stat__icon {
+		font-size: 2.5rem;
+		position: absolute;
+		bottom: 0.25rem;
+		right: 0.25rem;
+		opacity: 0.15;
+
+		@media (min-width: 640px) {
+			font-size: 3rem;
+		}
+	}
+</style>
