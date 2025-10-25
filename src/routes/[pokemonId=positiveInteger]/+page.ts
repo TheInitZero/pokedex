1281e9cd9010266d2060525cf2db74df0bd02984
TheInitZero <238483446+TheInitZero@users.noreply.github.com>
2025-10-25 11:39:08 +0530

    Bug fix: Use 'db.getPokemonById' instead of 'getPokemonById'.

--- src/routes/[pokemonId=positiveInteger]/+page.ts
+++ src/routes/[pokemonId=positiveInteger]/+page.ts
@@ -1,13 +1,9 @@
-import { getPokemomById } from '$lib/data/db';
+import { db } from '$lib/data/db';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async function load(event) {
-	const pokemon = getPokemomById(Number(event.params.pokemonId));
-
+	const pokemon = db.getPokemonById(Number(event.params.pokemonId));
 	if (pokemon == null) return error(404, { message: 'Pokémon not found' });
-
-	return {
-		pokemon
-	};
+	return { pokemon };
 };
